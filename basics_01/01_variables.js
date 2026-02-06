0bc7358125581b5aa90e9367ee645b0b97aee271
prathmesh-gh <majagaonkar0@gmail.com>
2026-02-06 15:44:01 +0000

    Variables Let,var and const

---
+++ basics_01/01_variables.js
@@ -0,0 +1,18 @@
+const accountld = 144553
+let accountEmaiI = "Pm@google.com"
+var accountPassword = "12345"
+accountCity ="Pune" // even if we dont assign the let const and var to variable javascript consider as a veriable
+let accountState;   // if we only declaired variables and not use it javascript takes it as a undefined
+
+// accountld = 1
+ accountEmaiI = "mp@google.com"
+ accountPassword = "000345"
+ accountCity="kolhapur"
+
+
+// console. log (accountld) ;
+// console.log(accountEmaiI);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountld,accountEmaiI,accountPassword,accountCity,accountState]);
