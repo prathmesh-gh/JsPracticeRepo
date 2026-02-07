061b59a55e5098382259f08e7af369e865484e13
prathmesh-gh <majagaonkar0@gmail.com>
2026-02-07 11:52:31 +0000

    Data Types

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,32 @@
+/**/
+"use strict"; // This treat all code as a newer version of js
+
+// alert(3+3); // we are using nodejs not browser
+
+console.log(3 +
+     3);        // code readiablity should be high
+
+console.log("Prathmesh");
+
+
+let name = "Prathmesh"
+let age = 18
+let isLoggedIn = false
+let state = null        
+
+
+// number/int => 2 to the power of 
+// bigint
+// string => "" or ''
+// boolean = Yes/No
+// null => standalone value 
+// undefined =>
+// symbol => for find Uniqness 
+
+
+//object
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // Object
+
+
